de0014ed7203bd5bfb52776ceda3ffa700e0166c
abtiwary <ab.tiwary@gmail.com>
2022-07-17 21:43:22 +1000

    Add command line parsing
    Now uses clap to read arguments from the command-line.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,48 @@
 use std::fs::File;
 use std::io::prelude::*;
 
+use clap::{Arg, Command};
 use anyhow::Result;
 
 use wave_gen_rs::{oscillator, wave};
 
 fn main() -> Result<()> {
-    let frequency: f64 = 440.0;
-    let amplitude: f64 = 0.5;
-    let phase: f64 = 0.0;
-    let sample_rate: usize = 44100;
-    let bit_depth: u16 = 8;
-    let max_amplitude_for_bit_depth: u16 = (2 << (bit_depth - 1)) - 1;
-    let duration: usize = 2;
+    let matches = Command::new("Simple WAVe Generator")
+        .version("0.1")
+        .author("Ab Tiwary")
+        .about("A simple wave generator in Rust")
+        .arg(Arg::new("frequency"))
+            .long_flag("frequency")
+        .arg(Arg::new("amplitude"))
+            .long_flag("amplitude")
+        .arg(Arg::new("phase"))
+            .long_flag("phase")
+        .arg(Arg::new("sample_rate"))
+            .long_flag("sample_rate")
+        .arg(Arg::new("bit_depth"))
+            .long_flag("bit_depth")
+        .arg(Arg::new("duration"))
+            .long_flag("duration")
+        .arg(Arg::new("outfile"))
+            .long_flag("outfile")
+        .get_matches();
+
+    // example values:
+    // frequency: 440.0
+    // amplitude: 0.5
+    // phase: 0.0
+    // sample_rate: 44100
+    // bit_depth: 8
+
+    let frequency: f64 = matches.value_of("frequency").unwrap().parse::<f64>().unwrap();
+    let amplitude: f64 = matches.value_of("amplitude").unwrap().parse::<f64>().unwrap();
+    let phase: f64 = matches.value_of("phase").unwrap().parse::<f64>().unwrap();
+    let sample_rate: usize = matches.value_of("sample_rate").unwrap().parse::<usize>().unwrap();
+    let bit_depth: u8 = matches.value_of("bit_depth").unwrap().parse::<u8>().unwrap();
+    let max_amplitude_for_bit_depth: u8 = (2 << (bit_depth - 1)) - 1;
+    let duration: usize = matches.value_of("duration").unwrap().parse::<usize>().unwrap();
+    
+    let outfile: String = matches.value_of("outfile").unwrap().parse::<String>().unwrap();
 
     let new_oscillator = oscillator::SineWaveOscillator::new(
         frequency, 
@@ -26,7 +56,7 @@ fn main() -> Result<()> {
         samples[i] = (&new_oscillator.tick(i) * (max_amplitude_for_bit_depth as f64)) as u8;
     }
 
-    let mut file = File::create("/Users/abtiwary/temp/test.wav")
+    let mut file = File::create(&outfile)
         .expect("fatal error: expected to create a file");
     let wave_header = wave::generate_wave_header(samples.len(), sample_rate as u32);
     file.write_all(&wave_header).expect("fatal error: expected to write wave header to file");;
@@ -34,3 +64,6 @@ fn main() -> Result<()> {
 
     Ok(())
 }
+
+// example usage: ./wave-gen-rs 440.0 0.5 0.0 44100 8 4 /Users/abtiwary/temp/test4.wav
+
